a1922389d11f615cfb9bbbfda766e6a1d03e6f99
Morten Paulsen <morten.p96@gmail.com>
2020-05-31 20:32:17 +0200

    Network able to send new_order commands with acknowlegdement.

--- lib/elevator/network.ex
+++ lib/elevator/network.ex
@@ -4,7 +4,7 @@ defmodule Elevator.Network do
   """
   use GenServer
 
-  alias Elevator.{Network.State, OrderController, OrderController.Order}
+  alias Elevator.{Network.State, Network.Packet, OrderController, OrderController.Order}
 
   @name Network
 
@@ -32,27 +32,67 @@ defmodule Elevator.Network do
     GenServer.cast(@name, {:new_node, node})
   end
 
+  @doc """
+  New order received from OrderController. Send to GenServer as a cast.
+  """
+  def new_order(%Order{} = order) do
+    GenServer.cast(@name, {:new_order, order})
+  end
+
   @impl true
   def handle_cast({:new_node, node}, %State{connection_pool: connection_pool} = state) do
     connection_pool = connection_pool_push(connection_pool, node)
     {:noreply, %State{state | connection_pool: connection_pool}}
   end
 
+  @impl true
   @doc """
-  Handle a timeout message. Remove the timed out connection.
+  Distribute the new order to all connected nodes.
+  Create a timer 
   """
-  def handle_info({:timeout, node}, %State{connection_pool: connection_pool} = state) do
-    connection_pool = connection_pool_pop(connection_pool, node)
-    {:noreply, %State{state | connection_pool: connection_pool}}
+  def handle_cast({:new_order, %Order{} = order}, %State{pending: pending} = state) do
+    new_pending =
+      for node <- all_nodes() do
+        packet = Packet.new() |> Packet.update([:source, :payload], [Node.self(), order])
+
+        IO.puts("Network: Sending order (#{packet.id}) to #{node}.")
+        Process.send({@name, node}, {:new_order, packet}, [:noconnect])
+        # Create a timer for each order and append to pending orders
+        Process.send_after(@name, {:timeout, packet}, state.timeouts.order)
+        packet
+      end
+
+    IO.inspect(new_pending)
+
+    pending = pending ++ new_pending
+    {:noreply, %State{state | pending: pending}}
+  end
+
+  @impl true
+  @doc """
+  Handle the new order by sending it to the OrderController. Create a timer
+  and expect a reply
+  """
+  def handle_info({:new_order, %Packet{} = packet}, %State{pending: pending} = state) do
+    IO.puts("New order received")
+    OrderController.new_order(packet.payload)
+    acknowledge_packet(:new, packet)
+    {:noreply, %State{state | pending: pending}}
+  end
+
+  def handle_info({:acknowledge, id}, %State{pending: pending} = state) do
+    IO.puts("Network: Packet #{id} acknowledged.")
+    pending = Enum.reject(pending, fn packet -> packet.id == id end)
+    {:noreply, %State{state | pending: pending}}
   end
 
   @doc """
-  Handle a new order from another elevator. Distribute it to `OrderController`.
+  Handle a timeout message. Remove the timed out connection.
   """
-  def handle_info({:new_order, order}, %State{pending: pending} = state) do
-    OrderController.new_order(order)
-    pending = acknowledge_order(:new, pending, order)
-    {:noreply, %State{state | pending: pending}}
+  @impl true
+  def handle_info({:timeout, node}, %State{connection_pool: connection_pool} = state) do
+    connection_pool = connection_pool_pop(connection_pool, node)
+    {:noreply, %State{state | connection_pool: connection_pool}}
   end
 
   @doc """
@@ -71,48 +111,40 @@ defmodule Elevator.Network do
     {:noreply, state}
   end
 
-  @impl true
-  @doc """
-  Return the GenServer state, used for debugging purposes
-  """
-  def handle_call(:get, _from, state) do
-    {:reply, state, state}
-  end
-
-  def handle_call(:pending, _from, state) do
-    {:reply, state.pending, state}
-  end
-
   defp connection_pool_push(connection_pool, node) do
-    IO.puts("Network: Connecting to new node #{node}.")
-    Node.connect(node)
-    Enum.concat(connection_pool, [node]) |> Enum.uniq()
+    case Enum.all?(connection_pool, fn x -> x != node end) do
+      true ->
+        IO.puts("Network: Connecting to new node #{node}.")
+        Node.connect(node)
+        Enum.concat(connection_pool, [node])
+
+      false ->
+        connection_pool
+    end
   end
 
   defp connection_pool_pop(connection_pool, node) do
-    IO.puts("Network: Disconnecting from node #{node}.")
-    Node.disconnect(node)
-    Enum.reject(connection_pool, fn conn -> conn == node end)
+    case Enum.any?(connection_pool, fn conn -> conn == node end) do
+      true ->
+        IO.puts("Network: Disconnecting from node #{node}.")
+        Node.disconnect(node)
+        Enum.reject(connection_pool, fn conn -> conn == node end)
+
+      false ->
+        connection_pool
+    end
   end
 
-  defp acknowledge_order(:new, %State{pending: pending} = state, %Order{} = new_order) do
-    pending =
-      case Enum.any?(pending, fn order -> order.id == new_order.id end) do
-        true ->
-          pending
-
-        false ->
-          pending ++ [new_order]
-      end
-
-    %State{state | pending: pending}
+  defp acknowledge_packet(:new, %Packet{id: id, source: source}) do
+    IO.puts("Network: Acknowledge order with id #{id}.")
+    Process.send({@name, source}, {:acknowledge, id}, [:noconnect])
   end
 
   # Credited: Jostein Løwer. https://github.com/jostlowe/kokeplata/tree/master/lib (27.05.20)
   defp all_nodes() do
     case [Node.self() | Node.list()] do
       [:nonode@nohost] -> {:error, :node_not_running}
-      nodes -> {:ok, nodes}
+      nodes -> nodes
     end
   end
 end
---
+++ lib/elevator/network/packet.ex
@@ -0,0 +1,47 @@
+defmodule Elevator.Network.Packet do
+  @moduledoc """
+  A struct for a network packet.
+
+  `id`: Unique ID defining the packet
+
+  `status`: An atom defining type of packet, one of `[:heartbeat, :timeout, :new_order, :update_order, :complete_order]`
+
+  `source`: Sender of the packet
+
+  `target`: Receiver of the packet
+
+  `payload`: Packet content
+  """
+
+  defstruct [:id, :source, :payload]
+
+  alias Elevator.Network.Packet
+
+  @doc """
+  Creates a new `%Elevator.Network.Packet{}`, setting its `id`.
+  """
+  def new() do
+    %Packet{id: UUID.uuid4(:hex)}
+  end
+
+  def update(%Packet{} = packet, keys, values) when is_list(keys) and is_list(values) do
+    Enum.zip(keys, values) |> Map.new() |> (fn x -> Map.merge(packet, x) end).()
+  end
+
+  @doc """
+  Assign a unique ID to `packet`.
+  """
+  def assign_id(%Packet{} = packet) do
+    case packet.id do
+      nil -> %Packet{packet | id: UUID.uuid4(:hex)}
+      _ -> packet
+    end
+  end
+
+  @doc """
+  Checks if the packet is valid.
+  """
+  def valid?(%Packet{} = packet) do
+    Enum.all?(Map.values(packet), fn v -> v != nil end)
+  end
+end
