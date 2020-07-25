4865d01e830faf1fad7365367e75d0393a2c0155
Morten Paulsen <morten.p96@gmail.com>
2020-07-26 00:16:09 +0200

    Add StateMachine and move light control to OrderController

--- lib/elevator/order_controller.ex
+++ lib/elevator/order_controller.ex
@@ -7,41 +7,46 @@ defmodule Elevator.OrderController do
   require Logger
 
   alias Elevator.OrderController.Order
+  alias Elevator.Driver
 
   def start_link(opts \\ {}) do
+    Logger.info("Starting GenServer")
     GenServer.start_link(__MODULE__, opts, name: OrderController)
   end
 
   def init(_opts) do
+    Logger.info("Initializing GenServer")
     state = {}
     {:ok, state}
   end
 
   def new_order(%Order{} = order) do
-    Logger.debug("New order received")
     GenServer.cast(OrderController, {:new_order, order})
   end
 
   def update_order(%Order{} = order) do
-    Logger.debug("Update order received")
     GenServer.cast(OrderController, {:update_order, order})
   end
 
   def complete_order(%Order{} = order) do
-    Logger.debug("Complete order received")
     GenServer.cast(OrderController, {:complete_order, order})
   end
 
   def handle_cast({:new_order, %Order{} = order}, state) do
+    Logger.debug("New Order")
     Elevator.StateMachine.new_order(order)
+    Driver.set_order_button_light(order.button, order.floor, :on)
     {:noreply, state}
   end
 
   def handle_cast({:update_order, %Order{} = order}, state) do
+    Logger.debug("Update Order")
     {:noreply, state}
   end
 
   def handle_cast({:complete_order, %Order{} = order}, state) do
+    Logger.debug("Complete Order")
+    Driver.set_order_button_light(order.button, order.floor, :off)
     {:noreply, state}
   end
 end
--- lib/elevator/state_machine.ex
+++ lib/elevator/state_machine.ex
@@ -9,64 +9,116 @@ defmodule Elevator.StateMachine do
   require Logger
 
   alias Elevator.StateMachine.{State, Timer}
+  alias Elevator.OrderController
   alias Elevator.OrderController.Order
   alias Elevator.Driver
 
+  # Initialization
+  
   def start_link(opts \\ {}) do
+    Logger.info("Starting GenServer")
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
   def init(_opts) do
+    Logger.info("Initializing GenServer")
     state = %State{
       floor: -1,
       direction: :stop,
       behaviour: :idle,
       orders: [],
-      lights: []
+      timeout_door: 3_000
     }
+
     {:ok, state}
   end
+
+  # API
 
   def new_order(%Order{} = order) do
     GenServer.cast(__MODULE__, {:new_order, order})
   end
 
-  def complete_order(%Order{} = order) do
-   GenServer.cast(__MODULE__, {:complete_order, order}) 
-  end
-
-  def arrive_at_floor(floor) do
+  def arrive_at_floor(floor) when is_integer(floor) do
     GenServer.cast(__MODULE__, {:arrive_at_floor, floor})
   end
 
-  def set_light(%Order{} = order) do
-    GenServer.cast(__MODULE__, {:set_light, order})
-  end
-
-  def clear_light(%Order{} = order) do
-    GenServer.cast(__MODULE__, {:clear_light, order})
-  end
+  # Handle functions
 
   def handle_cast({:new_order, %Order{} = order}, %State{orders: orders} = state) do
-    {:noreply, state}
+    already_active = order_active?(orders, order)
+
+    if already_active do
+      Logger.debug("Order already active")
+      {:noreply, state}
+    else
+      Logger.debug("New order")
+      state =
+        case state.behaviour do
+          :idle ->
+            if state.floor == order.floor do
+              open_door(state.timeout_door)
+              Map.put(state, :behaviour, :door_open)
+            else
+              state
+              |> add_order(order)
+              |> set_direction()
+              |> Map.put(:behaviour, :moving)
+            end
+
+          :moving ->
+            state
+            |> add_order(order)
+
+          :door_open ->
+            if state.floor == order.floor do
+              open_door(state.timeout_door)
+              state
+            else
+              state
+              |> add_order(order)
+            end
+
+          _ ->
+            state
+        end
+
+      {:noreply, state}
+    end
   end
 
   def handle_cast({:arrive_at_floor, new_floor}, %State{} = state) do
-    state = 
+    state =
       cond do
-        state.floor == -1 and state.direction == :stop ->
-          direction = start_between_floors()
-          %State{state | direction: direction}
         new_floor == -1 ->
-          # Do nothing, between floors
-          state
+          if state.floor == -1 and state.direction == :stop do
+            Logger.debug("Starting between floors")
+            start_between_floors(state)
+          else
+            state
+          end
+
         new_floor != state.floor ->
-          Logger.debug("Arrived at new floor #{new_floor}")
-          floor = new_floor
-          orders = update_orders(state.orders, floor)
-          Driver.set_floor_indicator(floor)
-          open_door(3000)
-          %State{state | floor: floor, orders: orders}
+          Logger.debug("New floor")
+          state = Map.put(state, :floor, new_floor)
+          Driver.set_floor_indicator(new_floor)
+
+          case state.behaviour do
+            :moving ->
+              if stop?(state) do
+                open_door(state.timeout_door)
+
+                state
+                |> remove_orders()
+                |> Map.put(:behaviour, :door_open)
+              else
+                state
+              end
+
+            _ ->
+              state
+          end
+
         true ->
           state
       end
@@ -74,43 +126,135 @@ defmodule Elevator.StateMachine do
     {:noreply, state}
   end
 
-  def handle_cast({:set_light, %Order{} = order}, %State{lights: lights} = state) do
-    Driver.set_order_button_light(order.button, order.floor, 1)
-    lights = put_in(lights, [order.floor, order.button], 1)
-
-    {:noreply, %State{state | lights: lights}}
-  end
-
-  def handle_cast({:clear_light, %Order{} = order}, %State{lights: lights} = state) do
-    Driver.set_order_button_light(order.button, order.floor, 0)
-    lights = put_in(lights, [order.floor, order.button], 0)
-
-    {:noreply, %State{state | lights: lights}}
-  end
-
   def handle_info(:timeout, %State{} = state) do
-    Logger.debug("Timer timeout")
-    close_door() 
+    Logger.debug("Closing door")
+    close_door()
+
+    state =
+      state
+      |> set_direction()
+      |> (fn s ->
+        if s.direction == :stop do
+          Map.put(s, :behaviour, :idle)
+        else
+          Map.put(s, :behaviour, :moving)
+        end
+      end).()
+
     {:noreply, state}
   end
 
-  defp start_between_floors() do
-    Logger.info("Starting between floors")
-    :down
+  # Helper functions
+
+  defp start_between_floors(%State{} = state) do
+    Driver.set_motor_direction(:down)
+    state
+    |> Map.put(:direction, :down)
+    |> Map.put(:behaviour, :moving)
   end
 
-  defp update_orders(orders, floor) do
-    Logger.debug("Updating orders")
-    Enum.filter(orders, fn order -> order.floor != floor end)
+  defp set_direction(%State{} = state) do
+    any_above = orders_above?(state.orders, state.floor)
+    any_below = orders_below?(state.orders, state.floor)
+
+    direction =
+      case state.direction do
+        :up ->
+          cond do
+            any_above -> :up
+            any_below -> :down
+            true -> :stop
+          end
+
+        :down ->
+          cond do
+            any_below -> :down
+            any_above -> :up
+            true -> :stop
+          end
+
+        :stop ->
+          cond do
+            any_below -> :down
+            any_above -> :up
+            true -> :stop
+          end
+
+        _ ->
+          :stop
+      end
+
+    Logger.debug("Setting direction to #{direction}")
+    Driver.set_motor_direction(direction)
+    %State{state | direction: direction}
+  end
+
+  defp stop?(%State{} = state) do
+    case state.direction do
+      :up ->
+        order_active?(state.orders, state.floor, :hall_up) or
+          order_active?(state.orders, state.floor, :cab) or
+          not orders_above?(state.orders, state.floor)
+
+      :down ->
+        order_active?(state.orders, state.floor, :hall_down) or
+          order_active?(state.orders, state.floor, :cab) or
+          not orders_below?(state.orders, state.floor)
+
+      :stop ->
+        true
+    end
+  end
+
+  defp orders_above?(orders, floor) do
+    Enum.any?(orders, fn order -> order.floor > floor end)
+  end
+
+  defp orders_below?(orders, floor) do
+    Enum.any?(orders, fn order -> order.floor < floor end)
+  end
+
+  defp order_active?(orders, %Order{floor: floor, button: button}) do
+    order_active?(orders, floor, button)
+  end
+
+  defp order_active?(orders, floor, button) do
+    Enum.any?(orders, fn order -> order.floor == floor and order.button == button end)
+  end
+
+  defp add_order(%State{orders: orders} = state, %Order{} = order) do
+    new_orders = Enum.uniq(orders ++ [order])
+    %State{state | orders: new_orders}
+  end
+
+  defp remove_order(%State{orders: orders} = state, %Order{} = order) do
+    new_orders = Enum.reject(orders, fn ord -> ord == order end)
+    %State{state | orders: new_orders}
+  end
+
+  defp remove_orders(%State{orders: orders, floor: floor} = state) do
+    new_orders =
+      Enum.reject(orders, fn order ->
+        if order.floor == floor do
+          OrderController.complete_order(order)
+          true
+        else
+          false
+        end
+      end)
+
+    %State{state | orders: new_orders}
   end
 
   defp open_door(timeout) do
+    Logger.debug("Opening door")
     Driver.set_motor_direction(:stop)
     Driver.set_door_open_light(:on)
     Timer.start(self(), timeout)
   end
 
   defp close_door() do
+    Logger.debug("Closing door")
     Driver.set_door_open_light(:off)
     Timer.stop()
   end
